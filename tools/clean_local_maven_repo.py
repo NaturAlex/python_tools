ba73bc64df8ff941f3198c70d6abb2c9eb753ba0
eaazhel <alex.c.zhang@ericsson.com>
2021-12-06 19:24:46 +0800

    revert py scripts

--- tools/clean_local_maven_repo.py
+++ tools/clean_local_maven_repo.py
@@ -5,31 +5,18 @@ python ./remove_local_dependency.py
 '''
 delete = False
 DEBUG = True
-<<<<<<< HEAD
-REPO_PATH="C:\\repository\\com\\ericsson\\"
-# REPO_PATH = "C:\\repository\\com\\ericsson\\nsi\\iam\\v2\\Hero"
-wait_delete_file = set()
-
-
-def findFile(path, keywords):
-=======
 # REPO_PATH="C:\\repository\\com\\ericsson\\"
 REPO_PATH="C:\\repository\\com\\ericsson\\nsi\\iam\\v2\\Hero"
 wait_delete_file = set()
 
 
 def findFile(path,keywords):
->>>>>>> 46667e3 (init)
     file_list = os.listdir(path)  
     for file_name in file_list:
         file_abs_path = os.path.join(path, file_name)
         if os.path.isdir(file_abs_path):
             # print('dir:'+file_abs_path)
-<<<<<<< HEAD
-            findFile(file_abs_path, keywords)
-=======
             findFile(file_abs_path,keywords)
->>>>>>> 46667e3 (init)
         else:
             if all(str in file_name for str in keywords):
                 wait_delete_file.add(path)
@@ -45,11 +32,7 @@ def deletefiles(wait_delete_file_set):
         if not DEBUG:
             os.remove(f)
             
-<<<<<<< HEAD
-def main(depend, version, debug, keyword_array):
-=======
 def main(depend,version,debug,keyword_array):
->>>>>>> 46667e3 (init)
     # keyword='autotest'
     # version='all'
     # keywords=['token-validation']
@@ -59,17 +42,9 @@ def main(depend,version,debug,keyword_array):
         keywords.append(version)
     else:
         keywords = keyword_array
-<<<<<<< HEAD
     DEBUG = debug
     findFile(REPO_PATH, keywords)
-=======
-    DEBUG=debug
-    findFile(REPO_PATH,keywords)
->>>>>>> 46667e3 (init)
-    deletefiles(wait_delete_file)
-
-"""
-python tools\clean_local_maven_repo.py --dependency Hero --version r21.8 --debug true
+''' 
 python tools\clean_local_maven_repo.py --help
 python tools\clean_local_maven_repo.py --keyword Hero --keyword r21.8 --debug true
 
@@ -88,22 +63,3 @@ except Exception as e:
 depend = args.depend
 version = args.version
 debug  = args.debug
-keyword_array = args.keywords
-if __name__ == "__main__":
-    try:
-        main(depend, version, debug, keyword_array)
-=======
-depend=args.depend
-version=args.version
-debug=args.debug
-keyword_array=args.keywords
-if __name__ == "__main__":
-    try:
-        main(depend,version,debug,keyword_array)
->>>>>>> 46667e3 (init)
-    except Exception as e:
-        print(e)
-    
-
-
-
